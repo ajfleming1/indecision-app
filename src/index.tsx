8c276c085d92bb9a12df3ec657d5f8f83ae42642
drew <dfleming@trackabout.com>
2020-04-29 08:52:48 -0400

    Chapter 3.12

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,28 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
+let count:number = 0;
+
+const updateCount = (i: number) => {
+    console.log(`Adding ${i} to count.`);
+    count += i;
+    renderApp();
+};
+
+const renderApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={() => updateCount(1)}>+1</button>
+            <button onClick={() => updateCount(-1)}>-1</button>
+            <button onClick={() => updateCount(count * -1)}>Reset</button>
+        </div>
+    );
+
+    ReactDOM.render(
+        templateTwo,
+        document.getElementById("example")
+    );
+}
+
+renderApp();
