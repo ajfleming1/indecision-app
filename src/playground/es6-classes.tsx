55f5c830fdfcb9011afb17dd3ebbf3b7cfb938b6
drew <dfleming@trackabout.com>
2020-04-30 12:22:12 -0400

    Chapter 4.4.

--- src/playground/es6-classes.tsx
+++ src/playground/es6-classes.tsx
@@ -11,7 +11,7 @@ class Person {
     };
 
     getGreeting(): string {
-        return `Hi ${this.name}.`;
+        return `Hi. I'm ${this.name}.`;
     }
 
     getDescription(): string {
@@ -19,11 +19,48 @@ class Person {
     }
 };
 
-const me = new Person("Drew Fleming", 36);
+class Student extends Person {
+    major: string;
+    constructor(name?: string, age?: number, major?: string) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription():string {
+        return `${super.getDescription()} They studied ` +
+               `${this.hasMajor() ? this.major : "something"}.`;
+    }
+};
+
+class Traveler extends Person {
+    homeLocation: string;
+    constructor(name?: string, age?: number, location?: string) {
+        super(name, age);
+        this.homeLocation = location;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if(this.homeLocation) {
+            greeting += ` I am currently visting ${this.homeLocation}.`
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Student("Drew Fleming", 36, "Computer Science");
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Student();
 console.log(other.getDescription());
+
+const traveler = new Traveler("Waldo", 40, "the moon");
+console.log(traveler.getGreeting());
 
 ReactDOM.render(
     <h1>{`Name: ${me.name}. Age: ${me.age}`}</h1>,
