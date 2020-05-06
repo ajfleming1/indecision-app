79d5b073a245bbceed23f4e98a251f3d7b3309bd
drew <dfleming@trackabout.com>
2020-05-06 09:44:06 -0400

    Chapter 5.3

--- src/playground/counter-example.tsx
+++ src/playground/counter-example.tsx
@@ -1,12 +1,24 @@
-const counterInitialState = {count: 0};
-type CounterState = Readonly<typeof counterInitialState>;
+export {};
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-class Counter extends React.Component<object, CounterState> {
-    readonly CounterState: CounterState = counterInitialState;
+const initialState = {
+    count: 0,
+    withStartCount(count: number){
+        this.count = count;
+        return this;
+    }
+};
+
+type State = Readonly<typeof initialState>;
+
+class Counter extends React.Component<{count: number}, State> {
+    static defaultProps: { count: number; };
+    readonly state: State = initialState.withStartCount(this.props.count);
     render() {
         return (
             <div>
-                <h1>Count: {this.CounterState.count}</h1>
+                <h1>Count: {this.state.count}</h1>
                 <button onClick={this.handleAddOne}>+1</button>
                 <button onClick={this.handleMinusOne}>-1</button>
                 <button onClick={this.handleReset}>Reset</button>
@@ -19,9 +31,13 @@ class Counter extends React.Component<object, CounterState> {
     handleReset = () => this.setState(resetCount);
 }
 
-const incrementCount = (prevCounterState: CounterState) => ({ count: prevCounterState.count + 1});
-const decrementCount = (prevCounterState: CounterState) => ({ count: prevCounterState.count - 1});
+const incrementCount = (prevState: State) => ({ count: prevState.count + 1});
+const decrementCount = (prevState: State) => ({ count: prevState.count - 1});
 const resetCount = () => ({ count: 0});
+
+Counter.defaultProps = {
+    count: 0
+}
 
 ReactDOM.render(<Counter />, document.getElementById("appRoot"));
 
