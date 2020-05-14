1d3c2450bb44e2366135220afb4ddb9a6a4e3a92
drew <dfleming@trackabout.com>
2020-05-14 15:26:07 -0400

    Chapter 7.2

--- src/app.tsx
+++ src/app.tsx
@@ -2,4 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("appRoot"));
+const Layout = (props: { children: React.ReactNode; }) => (
+    <div>
+        <p>Header</p>
+        {props.children}
+        <p>Footer</p>
+    </div>
+);
+
+const Template = (
+    <div>
+        <h1>Title</h1>
+        <p>This is my page.</p>
+    </div>
+);
+
+ReactDOM.render(<Layout><p>This is inline. {Template}</p></Layout>, document.getElementById("appRoot"));
